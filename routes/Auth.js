db5722ed1e67936afcd0c278a042bf87f4e14dd5
vijaygohil1223 <vijayg.kanhasoft@gmail.com>
2023-08-29 19:45:22 +0530

    changes in auth api

--- routes/Auth.js
+++ routes/Auth.js
@@ -3,6 +3,7 @@ const { CreateUser ,LoginUser } = require('../controller/Auth');
 
 const router = express.Router();
 
-router.post('/signup', CreateUser).post('login',LoginUser)
+router.post('/signup', CreateUser)
+router.post('/login',LoginUser)
 
 exports.router = router;
